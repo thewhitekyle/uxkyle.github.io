b2522abb208fbb7256bea1227cb0de23177e421d
Kyle White <thewhitekyle@gmail.com>
2018-07-10 16:44:54 -0400

    Homebrew/Yarn/Webpack Install

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: [
+    "webpack-hot-client/client?http://localhost:8080",
+    "webpack/hot/only-dev-server",
+    "./src/App.js"
+  ],
+  output: {
+    path: path.join(__dirname, "./dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        exclude: /(node_modules)/,
+        use: "css-loader"
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ],
+  devServer: {
+    hot: true,
+    contentBase: "./src"
+  }
+};
