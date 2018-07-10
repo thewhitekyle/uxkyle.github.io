7550568731dbeec4f56005ec7dd0fe7a4dedcd53
Kyle White <thewhitekyle@gmail.com>
2018-07-10 16:49:07 -0400

    webpack github pages fix

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     "./src/App.js"
   ],
   output: {
-    path: path.join(__dirname, "./dist"),
+    path: path.join(__dirname, "./"),
     filename: "bundle.js"
   },
   module: {
