e5344eae25b67377e52b25609cbb9160795c1079
Kyle White <thewhitekyle@gmail.com>
2019-05-11 22:56:11 -0700

    Menu Overlay

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("menu").style.top = "0";
-  } else {
-    document.getElementById("menu").style.top = "-80px";
-  }
-  prevScrollpos = currentScrollPos;
-}
+$(document).ready(function () {
+
+  var prevScrollpos = window.pageYOffset;
+  window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("menu").style.top = "0";
+    } else {
+      document.getElementById("menu").style.top = "-80px";
+    }
+    prevScrollpos = currentScrollPos;
+  };
+
+    $('#toggle').click(function() {
+       $(this).toggleClass('active');
+       $('#overlay').toggleClass('open');
+      });
+
+});
